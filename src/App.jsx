3a2d299afa5bd7d104231a9df75318f0a69703d0
SanjanaVJadhav01 <sanjana.vjadhav03@gmail.com>
2025-10-16 10:18:51 +0530

    Updated Taskform.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import Progresstracker from "./Components/Progresstracker";
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/Tasklist";
 
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  const addTask = (task) => {
+    setTasks(task);
+  }
   return (
     <div>
       <h1>Task Focus</h1>
       <p>Our Friendly TaskManager</p>
-      <Taskform />
+      <Taskform addTask = {addTask}/>
       <TaskList />
       <Progresstracker />
       <button>Clear all tasks</button>
     </div>
   )
 }
-export default App;
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,20 +1,32 @@
-export default function Taskform() {
+import { useState } from "react"
+
+export default function Taskform({addTask}) {
+    const [task, setTask]= useState('');
+    const [priority, setPriority] = useState('medium');
+    const [category, setCategory] = useState('general');
+
+    const handlesubmit = () => {
+        addTask({text: task, priority, category, completed: false});
+    }
+
     return (
-        <form>
+        <form onSubmit={handlesubmit}>
             <div>
-                <input type="text" placeholder="Enter the task"/>
+                <input type="text" placeholder="Enter the task"
+                onChange={(e) => setTask(e.target.value)} />
                 <button type="submit">Add Task</button>
+                <h1>{task} {priority} {category}</h1>
             </div>
 
             <div>
-                <select>
+                <select onChange={(e) => setPriority(e.target.value)}>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
                     <option value="low">Low</option>
                 </select>
 
-                <select>
-                    <option value="generalgh">General</option>
+                <select onChange={(e) => setCategory(e.target.value)}>
+                    <option value="general">General</option>
                     <option value="work">Work</option>
                     <option value="personal">Personal</option>
                 </select>
